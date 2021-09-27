f139209095a6c213282a0edbdc5f56cd838bc894
MKS1998 <nksayyad2900@outlook.com>
2021-09-27 14:12:22 +0530

    Calculate Employee Part Time Wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -61,3 +61,42 @@ public static void CalculateDailyWages()
 }
 
 }
+
+
+/*  Calculate Employee Part Time Wages  */
+
+
+
+import java.util.Random;
+import java.util.*;
+
+
+public class EmployeeWages {
+	public static void main( String args[] ){
+		System.out.println("Welcome To The Employee Wages");
+		CalculateDailyWages();
+}
+
+public static void CalculateDailyWages()
+{
+	int IsFullTime = 1;
+	Random rn = new Random();
+	int check= rn.nextInt(2);
+	int RatePerHour = 20;
+	int PartTimeHour = 4;
+	int FullDayHour = 8;
+	int TotalWages = 0;
+	if(check == IsFullTime)
+	{
+		System.out.println("Employee Present.");
+		TotalWages = RatePerHour * PartTimeHour;
+		System.out.println("Daily PartTime Wages  : "+ TotalWages);
+		
+	}
+	else
+ 	{
+		System.out.println("Employee Absent.");
+	}
+}
+
+} 
