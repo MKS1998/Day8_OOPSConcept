14aedbd733d4eeb9e60f237a5af4f88de866d905
MKS1998 <nksayyad2900@outlook.com>
2021-09-27 14:09:49 +0530

    Calculate Employee Daily Wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -24,3 +24,40 @@ public static void Attendance()
 }
 
 } 
+
+
+
+/* UC2_Calculate Employee Daily Wages  */
+
+
+
+import java.util.Random;
+import java.util.*;
+public class EmployeeWages {
+	public static void main( String args[] ){
+		System.out.println("Welcome To The Employee Wages");
+		CalculateDailyWages();
+}
+
+public static void CalculateDailyWages()
+{
+	int IsFullTime = 1;
+	Random rn = new Random();
+	int check= rn.nextInt(2);
+	int RatePerHour = 20;
+	int FullDayHour = 8;
+	int TotalWages = 0;
+	if(check == IsFullTime)
+	{
+		System.out.println("Employee Present.");
+		TotalWages = RatePerHour * FullDayHour;
+		System.out.println("Daily Wages  : "+ TotalWages);
+		
+	}
+	else
+ 	{
+		System.out.println("Employee Absent.");
+	}
+}
+
+}
